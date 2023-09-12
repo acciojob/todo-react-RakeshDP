e7297ab5d30aedb850d8ff5baa74e3ec03aa718a
RakeshDP <rakeshpawar1895.rp@gmail.com>
2023-09-13 00:42:56 +0530

    submitting code-Wed Sep 13 2023 00:42:56 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -2,44 +2,47 @@ import React,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const [todos,setTodos]=useState([]);
-  const [newTodo, setNewTodo]= useState("");
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
   // Function to add a new todo item
-  const addTodo=()=>{
-    if(newTodo.trim() !== ""){
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
       setTodos([...todos, newTodo]);
-      setNewTodo("")
+      setNewTodo("");
     }
-  }
+  };
 
-  // Function to remove the todo items
-  const removeTodo =(index)=>{
-    const updatedTodos =todos.filter((_,i)=> i !== index);
+  // Function to remove a todo item
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
     setTodos(updatedTodos);
-  }
-
+  };
 
   return (
-    <div >
-        <h1>To-Do-List</h1>
+    <div>
+      <h2>To-Do List</h2>
       <div className="main">
-          <input type="text"  value={newTodo} onChange={(e)=>setNewTodo(e.target.value)}/>
-          <button onClick={addTodo}>Add Todo</button>
+        <input
+          type="text"
+          placeholder="Add a new task"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
       </div>
-             <div>
-             <ul>
-                {
-                  todos.map((todo,index)=>(
-                    <li key={index}>
-                      {todo}
-                      <button onClick={()=> removeTodo(index)}>Delete</button>
-                    </li>
-                  ))}
-              </ul>
-              </div> 
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
 
 export default App
