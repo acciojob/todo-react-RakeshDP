61274b0fea5324943802bb4755f1824e2c69ecea
RakeshDP <rakeshpawar1895.rp@gmail.com>
2023-09-13 00:32:06 +0530

    submitting code-Wed Sep 13 2023 00:32:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
-
-import React from "react";
+import React,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos,setTodos]=useState([]);
+  const [newTodo, setNewTodo]= useState("");
+
+  // Function to add a new todo item
+  const addTodo=()=>{
+    if(newTodo.trim() !== ""){
+      setTodos([...todos, newTodo]);
+      setNewTodo("")
+    }
+  }
+
+  // Function to remove the todo items
+  const removeTodo =(index)=>{
+    const updatedTodos =todos.filter((_,i)=> i !== index);
+    setTodos(updatedTodos);
+  }
+
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div >
+        <h1>To-Do-List</h1>
+      <div className="main">
+          <input type="text"  value={newTodo} onChange={(e)=>setNewTodo(e.target.value)}/>
+          <button onClick={addTodo}>Add Todo</button>
+      </div>
+             <div>
+             <ul>
+                {
+                  todos.map((todo,index)=>(
+                    <li key={index}>
+                      {todo}
+                      <button onClick={()=> removeTodo(index)}>Delete</button>
+                    </li>
+                  ))}
+              </ul>
+              </div> 
     </div>
   )
 }
